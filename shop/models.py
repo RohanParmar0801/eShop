1c41d84c22c42ce52bd46112b8968c50568970d4
Rohan Parmar <rohanrparmar987@gmail.com>
2022-08-07 19:54:46 +0530

    Change in Product Model

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from itertools import product
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
@@ -6,5 +7,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=20)
+    category = models.CharField(max_length=50)
+    sub_category = models.CharField(max_length=50)
+    price = models.IntegerField()
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
+    imege = models.ImageField(upload_to = "shop/images")
+
+    def __str__(self):
+        return self.product_name
