826b030280ccd0e7b20148b09c3a3e7c918ea74f
Rohan Parmar <rohanrparmar987@gmail.com>
2022-08-07 15:21:51 +0530

    Adding Shop URL's

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,10 @@ from shop import views as shop_views
 
 urlpatterns = [
     path('', shop_views.index, name="Shop Home"),
+    path('about/', shop_views.about, name="About Us"),
+    path('contact/', shop_views.contact, name="Contact Us"),
+    path('tracker/', shop_views.tracker, name="Tracking Status"),
+    path('search/', shop_views.search, name="Search"),
+    path('productview/', shop_views.product_view, name="Product View"),
+    path('checkout/', shop_views.checkout, name="Checkout"),
 ]
