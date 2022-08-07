b55637b5de85b2d27bdd5e684a0ef9c18918f2de
Rohan Parmar <rohanrparmar987@gmail.com>
2022-08-07 14:42:13 +0530

    Add Bootstrap Templates

--- eShop/settings.py
+++ eShop/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project APP's
+    'blog',
+    'shop',
 ]
 
 MIDDLEWARE = [
